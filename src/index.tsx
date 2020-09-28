e8a2c8993ce09a2b78b69b2e440e5815bc6cdcd9
Salman Zafar <maanizfar@gmail.com>
2020-09-28 14:38:15 +0500

    add styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+import App from "./app/App";
 import { setupServer } from "./services/mirage/server";
 import { Provider } from "react-redux";
 import store from "./store";
+import "./index.css";
 
 if (process.env.NODE_ENV === "development") {
   setupServer();
